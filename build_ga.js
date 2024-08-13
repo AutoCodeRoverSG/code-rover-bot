0604cb984c1a9d039fd9a650afe7614cb71f269a
Zhang Yuntong <ang.unong@gmail.com>
2024-08-13 18:54:51 +0800

    Update action

---
+++ build_ga.js
@@ -0,0 +1,20 @@
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
+    entryPoints: ['src/index_github.js'],
+    bundle: true,
+    outfile: 'dist/index.js',
+    format: "esm",
+    target: "esnext",
+    platform: "node",
+    loader: {".node" : 'copy'},
+    banner:{
+        js: `
+        import { fileURLToPath } from 'url';
+        import { createRequire as topLevelCreateRequire } from 'module';
+        const require = topLevelCreateRequire(import.meta.url);
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        `
+    },
+})
