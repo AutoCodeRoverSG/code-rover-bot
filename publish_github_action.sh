40297b2d0198260c095ee3f3d3cc46ceb491f505
Zhang Yuntong <ang.unong@gmail.com>
2024-08-13 12:42:20 +0800

    Update action

--- publish_github_action.sh
+++ publish_github_action.sh
@@ -4,5 +4,9 @@ npm run build-ga
 
 git add .
 git commit -m "Update action"
+
+git tag --delete v0.0.2
+git push --delete origin v0.0.2
+
 git tag -a -m "Update action" v0.0.2
 git push --follow-tags
