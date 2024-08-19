1aa7b21ce36293e83506798c46f5c23b45db8baf
Zhang Yuntong <ang.unong@gmail.com>
2024-08-19 16:01:41 +0800

    Update action

--- build_ga.js
+++ build_ga.js
@@ -14,6 +14,7 @@ await esbuild.build({
         import { createRequire as topLevelCreateRequire } from 'module';
         const require = topLevelCreateRequire(import.meta.url);
         const __filename = __fileURLToPath(import.meta.url);
+        const path = require('node:path');
         const __dirname = path.dirname(__filename);
         `
     },
